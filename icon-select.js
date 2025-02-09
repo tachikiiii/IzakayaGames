9da49e9fa76b4cc2509257e9b803bb8727c53ef7
Maya <ha4103m@gmail.com>
2025-02-09 15:27:55 +0900

    エラー解決

--- icon-select.js
+++ icon-select.js
@@ -63,8 +63,8 @@ function selectIcon(iconId) {
   // すべてのプレイヤー分選択済みの場合
   if (players.length === maxPlayers) {
     // ヘッダーを完了メッセージに変更
-    const header = document.querySelector("header h1");
-    header.textContent = "すべてのプレイヤーの登録が完了しました";
+    //const header = document.querySelector("header h1");
+    //header.textContent = "すべてのプレイヤーの登録が完了しました";
     // 「次へ」ボタンを表示し、クリック時にゲーム開始へ遷移
     const nextButton = document.querySelector("#next");
     nextButton.style.display = "inline-block";
