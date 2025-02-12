9f23e4e040a981a7a434fd95ceae28f9048bea87
Maya <ha4103m@gmail.com>
2025-02-12 12:10:14 +0900

    add title & changed to touch

--- home.js
+++ home.js
@@ -2,6 +2,5 @@ function redirectToNextPage() {
     window.location.href = "mode-select.html"; 
 }
 
-setTimeout(redirectToNextPage, 5000);
-
-document.body.addEventListener("click", redirectToNextPage);
+document.addEventListener('click', redirectToNextPage);
+//setTimeout(redirectToNextPage, 5000);
